3d9879069c76662594ea4b1730725019731f04dd
Christian Andrei Reyes <rchristianandrei@gmail.com>
2026-03-28 21:57:16 +0800

    create backend api

---
+++ backend/GlobalExceptionHandler.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+
+namespace backend;
+
+public class GlobalExceptionHandler : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext context,
+        Exception exception,
+        CancellationToken cancellationToken)
+    {
+        context.Response.StatusCode = 500;
+
+        await context.Response.WriteAsJsonAsync(new
+        {
+            message = "Global error handled",
+            error = exception.Message
+        }, cancellationToken);
+
+        return true;
+    }
+}
