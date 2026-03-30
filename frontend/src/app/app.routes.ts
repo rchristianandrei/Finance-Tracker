df47b5cf0e55976266af24388aad0d657fd63cc6
Christian Andrei Reyes <rchristianandrei@gmail.com>
2026-03-30 22:29:44 +0800

    create verify account route

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,4 +1,15 @@
 import { Routes } from '@angular/router';
-import { Register } from './pages/register/register';
 
-export const routes: Routes = [{ path: 'register', title: 'Register', component: Register }];
+export const routes: Routes = [
+  {
+    path: 'register',
+    title: 'Register',
+    loadComponent: () => import('./pages/register/register').then((c) => c.Register),
+  },
+  {
+    path: 'verify-account',
+    title: 'Verify Account',
+    loadComponent: () =>
+      import('./pages/verify-account/verify-account').then((c) => c.VerifyAccount),
+  },
+];
