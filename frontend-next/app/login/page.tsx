579b8ad45859aad49bab1248f0d23a9c66753acd
Christian Andrei Reyes <rchristianandrei@gmail.com>
2026-06-05 22:16:48 +0800

    add titles on routes

--- frontend-next/app/login/page.tsx
+++ frontend-next/app/login/page.tsx
@@ -1,6 +1,10 @@
 import { PublicRoute } from "@/components/guards/PublicRoute"
 import LoginForm from "./loginForm"
 
+export const metadata = {
+  title: "Login",
+}
+
 export default function LoginPage() {
   return (
     <PublicRoute>
