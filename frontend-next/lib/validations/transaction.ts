b305b371414d53719350b0254a1feb762262051e
Christian Andrei Reyes <rchristianandrei@gmail.com>
2026-06-13 21:50:21 +0800

    add category validation on create transaction

--- frontend-next/lib/validations/transaction.ts
+++ frontend-next/lib/validations/transaction.ts
@@ -3,7 +3,10 @@ import z from "zod"
 export const transactionSchema = z.object({
   type: z.enum(["1", "2"]),
 
-  category: z.string().min(1, "Category is required"),
+  category: z
+    .string()
+    .min(1, "Category is required")
+    .max(50, "Name must be 50 characters or less"),
 
   description: z.string().min(1, "Description is required"),
 
