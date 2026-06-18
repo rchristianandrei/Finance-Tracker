0b2b67e2952c7b009558eadb3203b9bc1a9ef375
Christian Andrei Reyes <rchristianandrei@gmail.com>
2026-06-18 23:00:27 +0800

    redirect api

--- frontend-next/lib/axios.ts
+++ frontend-next/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 import axiosRetry from "axios-retry"
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: "/api",
   withCredentials: true,
   timeout: 20000,
 })
