ac66aa43d4aa2b055823179ffb3100bd5a96200b
Christian Andrei Reyes <rchristianandrei@gmail.com>
2026-07-16 17:36:57 +0800

    improve ui

--- frontend-next/app/dashboard/components/vertical-bar-graph.tsx
+++ frontend-next/app/dashboard/components/vertical-bar-graph.tsx
@@ -1,5 +1,6 @@
 import { CategorySummary } from "@/types/dashboard"
 import { Card, CardContent } from "@/components/ui/card"
+import { cn } from "@/lib/utils"
 
 export function VerticalBarGraph({
   categorySummaries,
@@ -17,13 +18,22 @@ export function VerticalBarGraph({
             className="absolute inset-y-0 left-0 bg-destructive/20"
             style={{ width: `${categorySummary.percentage}%` }}
           />
-          <CardContent className="grid grid-cols-[1fr_10%_10%] *:text-destructive">
-            <div>{categorySummary.category}</div>
-            <div className="text-right">
-              {categorySummary.amount.toFixed(2)}{" "}
-            </div>
-            <div className="text-right">
-              {categorySummary.percentage.toFixed(2)}%
+          <CardContent className="grid grid-cols-[1fr_auto] items-center *:text-destructive">
+            <p className="truncate">{categorySummary.category}</p>
+            <div className="flex flex-col gap-1 text-right">
+              <div>
+                <span
+                  className={cn(
+                    "rounded-full px-2 py-1 font-medium dark:bg-red-900 dark:text-red-300"
+                  )}
+                >
+                  {categorySummary.amount.toLocaleString("en-US", {
+                    minimumFractionDigits: 2,
+                    maximumFractionDigits: 2,
+                  })}
+                </span>
+              </div>
+              <div>{categorySummary.percentage.toFixed(2)} %</div>
             </div>
           </CardContent>
         </Card>
