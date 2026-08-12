f7d26e6383fdbf83ba5089a8a5f9bc3dd9c6ad68
Christian Andrei Reyes <rchristianandrei@gmail.com>
2026-08-12 18:36:44 +0800

    add bottom border on layout

--- frontend-next/components/layouts/root-layout/root-layout.tsx
+++ frontend-next/components/layouts/root-layout/root-layout.tsx
@@ -15,7 +15,7 @@ export function RootLayout({ children }: { children: React.ReactNode }) {
       <AppSidebar />
       <AddTransactionProvider>
         <SidebarInset>
-          <header className="flex h-16 shrink-0 items-center justify-between gap-2 px-4 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+          <header className="flex h-16 shrink-0 items-center justify-between gap-2 border-b-2 px-4 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
             <div className="flex items-center gap-2">
               <SidebarTrigger className="-ml-1" />
               <Separator
